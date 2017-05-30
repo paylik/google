fa5594329292d5cea16bded1c2aa4f3d71b9c146
paylik <paylik@yandex.ru>
2017-05-30 10:40:02 +0300

    form js

--- scroll.js
+++ scroll.js
@@ -46,18 +46,6 @@
         }
         console.log("1")
     };
-
-
-    document.querySelector('.key').onmouseover = document.querySelector('.key').onmouseout = handler;
-
-    function handler(event) {
-
-        if (event.type == 'mouseover') {
-            event.target.style.background = 'pink'
-        }
-        if (event.type == 'mouseout') {
-            event.target.style.background = ''
-        }
-    }
+    
 
 })();
