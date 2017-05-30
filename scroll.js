2c5a1ec95fe51bdcb5d4262d49f7166c482fb13a
paylik <paylik@yandex.ru>
2017-05-30 14:49:46 +0300

    js

--- scroll.js
+++ scroll.js
@@ -1,7 +1,6 @@
 
 (function scroll() {
     var tools = document.querySelector('.header_app_1');
-    var check_1 = document.querySelector('#hd-1');
     var check_2 = document.querySelector('#hd-2');
 
     if (tools.addEventListener) {
@@ -19,14 +18,9 @@
         tools.attachEvent("onmousewheel", onWheel);
     }
 
-
-    // Это решение предусматривает поддержку IE8-
     function onWheel(e) {
         e = e || window.event;
 
-        // deltaY, detail содержат пиксели
-        // wheelDelta не дает возможность узнать количество пикселей
-        // onwheel || MozMousePixelScroll || onmousewheel
         var delta = e.deltaY || e.detail || e.wheelDelta;
 
         if (delta <= 0) {
@@ -37,15 +31,12 @@
 
     }
 
-
     document.onclick = function(e) {
         myDiv = document.getElementById('hd-1');
         if (e.target.id != myDiv.id)  {
             myDiv.checked = false;
             check_2.checked = false;
         }
-        console.log("1")
     };
-    
 
 })();
